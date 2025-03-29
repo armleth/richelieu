b5558418fe20b0b751c52b4bbc3b05155351b3a8
Armleth <armleth@proton.me>
2025-03-29 16:05:50 +0100

    feat: k3s addition

--- configuration.nix
+++ configuration.nix
@@ -1,64 +1,56 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
+    imports =
+        [
+        ./hardware-configuration.nix
+        ];
+
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "nixosVM";
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "Europe/Paris";
+
+    users.users.armleth = {
+        isNormalUser = true;
+        home = "/home/armleth";
+        extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    };
+
+    environment.systemPackages = with pkgs; [
+        neovim
+        wget
+        git
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    networking.firewall.allowedTCPPorts = [
+        6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    ];
+    networking.firewall.allowedUDPPorts = [
+# 8472 # k3s, flannel: required if using multi-node for inter-node networking
+    ];
+    services.k3s.enable = true;
+    services.k3s.role = "server";
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.armleth = {
-    isNormalUser = true;
-    home = "/home/armleth";
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-  };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim
-    wget
-    git
-  ];
-
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings = {
-      PasswordAuthentication = true;
-      AllowUsers = null;
-      UseDns = true;
-      X11Forwarding = false;
-      PermitRootLogin = "prohibit-password";
+    services.openssh = {
+        enable = true;
+        ports = [ 22 ];
+        settings = {
+            PasswordAuthentication = true;
+            AllowUsers = null;
+            UseDns = true;
+            X11Forwarding = false;
+            PermitRootLogin = "prohibit-password";
+        };
     };
-  };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  system.stateVersion = "24.11"; # Nixos version when this system was installed
+    system.stateVersion = "24.11"; # Nixos version when this system was installed
 }
 
