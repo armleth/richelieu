589115e85a1ad0e6257864867f63ac66c2d4687b
Armleth <armand.thibaudon@epita.fr>
2026-03-03 22:26:32 +0100

    fix(keycloak): adding the right groups

--- terraform/keycloak/groups.tf
+++ terraform/keycloak/groups.tf
@@ -3,21 +3,43 @@ resource "keycloak_group" "admins" {
   name     = "admins"
 }
 
-# Protocol mapper to include groups in the OIDC token
+# Client scope so that clients can request "groups" as an OIDC scope
+resource "keycloak_openid_client_scope" "groups" {
+  realm_id               = keycloak_realm.infrastructure.id
+  name                   = "groups"
+  description            = "Group membership"
+  include_in_token_scope = true
+}
+
+# Protocol mapper to include groups in the token when the scope is requested
 resource "keycloak_openid_group_membership_protocol_mapper" "groups" {
-  realm_id  = keycloak_realm.infrastructure.id
-  client_id = keycloak_openid_client.argocd.id
-  name      = "group-membership"
+  realm_id        = keycloak_realm.infrastructure.id
+  client_scope_id = keycloak_openid_client_scope.groups.id
+  name            = "group-membership"
 
   claim_name = "groups"
   full_path  = false
 }
 
-resource "keycloak_openid_group_membership_protocol_mapper" "vault_groups" {
+# Assign the groups scope to ArgoCD and Vault clients
+resource "keycloak_openid_client_default_scopes" "argocd" {
+  realm_id  = keycloak_realm.infrastructure.id
+  client_id = keycloak_openid_client.argocd.id
+  default_scopes = [
+    "openid",
+    "profile",
+    "email",
+    keycloak_openid_client_scope.groups.name,
+  ]
+}
+
+resource "keycloak_openid_client_default_scopes" "vault" {
   realm_id  = keycloak_realm.infrastructure.id
   client_id = keycloak_openid_client.vault.id
-  name      = "group-membership"
-
-  claim_name = "groups"
-  full_path  = false
+  default_scopes = [
+    "openid",
+    "profile",
+    "email",
+    keycloak_openid_client_scope.groups.name,
+  ]
 }
