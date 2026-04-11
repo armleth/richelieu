8c2d99dd40e3868bd566a0b61584bb5cb73781bf
Armleth <armand.thibaudon@epita.fr>
2026-04-11 18:57:45 +0200

    fix(code-server): right terraform groups

--- terraform/keycloak/groups.tf
+++ terraform/keycloak/groups.tf
@@ -70,3 +70,14 @@ resource "keycloak_openid_client_default_scopes" "homepage" {
     keycloak_openid_client_scope.groups.name,
   ]
 }
+
+resource "keycloak_openid_client_default_scopes" "code_server" {
+  realm_id  = keycloak_realm.infrastructure.id
+  client_id = keycloak_openid_client.code_server.id
+  default_scopes = [
+    "openid",
+    "profile",
+    "email",
+    keycloak_openid_client_scope.groups.name,
+  ]
+}
